9bbfabd5690acdebc7fd45945865dd79536ab0ca
Alexis <aaquinonez@teco.com.ar>
2025-07-03 17:18:06 -0300

    Initial Commit

---
+++ HabitTrackerSwift/HabitTrackerSwiftApp.swift
@@ -0,0 +1,17 @@
+//
+//  HabitTrackerSwiftApp.swift
+//  HabitTrackerSwift
+//
+//  Created by Alexis Quiñonez on 03/07/2025.
+//
+
+import SwiftUI
+
+@main
+struct HabitTrackerSwiftApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
